eb64ef8bb6e993662917be83e00c4a016118fe4f
Vlad <iwanow.wld@gmail.com>
2019-10-20 19:25:26 +0400

    4/73

--- src/index.js
+++ src/index.js
@@ -4,9 +4,40 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let result = 0;
+    expr = expr.replace(/\s/g, '');
+    function findAndCalc(op1, op2, operand) {
+        op1 = Number(op1)
+        op2 = Number(op2)
+        switch (operand) {
+            case '+':
+                return op1 + op2;
+                break;
+            case '-':
+                return op1 - op2;
+                break;
+            case '*':
+                return op1 * op2;
+                break;
+            case '/':
+                return op1 / op2;
+                break;
+        }
+    }
+    let exprArr = expr.split('');
+    let operators = ['+', '-', '*', '/']
+    for (let i = 0; i < exprArr.length; i++) {
+        if (operators.includes(exprArr[i])) {
+            if (exprArr[i] == '/' && exprArr[i + 1] == 0) {
+                throw "divide by zero";
+            }
+            else {
+                result = findAndCalc(exprArr[i - 1], exprArr[i + 1], exprArr[i]);
+            }
+        }
+    }
+    return result;
 }
-
 module.exports = {
     expressionCalculator
 }
